5f60a0603dbf19a83003ec2b08efac49454b0066
kusonekochansama <chatgpt@keyvis.jp>
2024-07-10 16:00:33 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const { Pool } = require('pg');
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
-
-const path = require('path');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: true,
-        ca: fs.readFileSync(path.join(__dirname, 'combined-certificates.crt')).toString(),
+        rejectUnauthorized: false, // 一時的に検証を無効化
     },
 });
 
@@ -21,10 +19,6 @@ pool.connect((err, client, release) => {
     }
     console.log('Database connected successfully');
     release();
-});
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
