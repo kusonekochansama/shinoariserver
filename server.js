83e2162ea1d0dccc6930d6c5b156d23c2385098f
kusonekochansama <chatgpt@keyvis.jp>
2024-07-10 16:38:09 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
+// DATABASE_URL を使ってプールを作成
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.postgres://kusonekochan:gJONrWmTJmoq2x46pTd5xOOxc8KytPJB@dpg-cp5vsoo21fec73ecvc5g-a.singapore-postgres.render.com:5432/suika, // ここでDATABASE_URLを使用
     ssl: {
         rejectUnauthorized: true,
         ca: fs.readFileSync('combined-certificates.crt').toString(),
@@ -22,7 +23,7 @@ pool.connect((err, client, release) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
